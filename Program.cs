c2da7969b2c69ac5a37a998535f061bd4456f29e
Slimane RECHDI <47180175+recs12@users.noreply.github.com>
2020-10-15 07:00:00 -0400

    Add files via upload
    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevOpsWorkItemsQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
