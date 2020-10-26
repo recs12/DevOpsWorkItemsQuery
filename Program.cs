461374127eb127233e727ba56dfd455882843287
slimane rechdi <slimane.rechdi@gmail.com>
2020-10-26 09:40:10 -0400

    refactoring

--- Program.cs
+++ Program.cs
@@ -6,22 +6,32 @@ namespace DevOpsWorkItemsQuery
 
     public class Program
     {
+
         static void Main()
         {
+            string __version__ = "0.0.0";
+            string __author__ = "recs";
+            string __update__ = "2020-10-23";
+            string __project__ = "DevOpsWorkItemsQuery";
+
+
+            Console.WriteLine(
+                $"{__project__}  --author: {__author__} --version: {__version__} --last-update :{__update__} ");
+
             Task.Run(async () =>
             {
-                string orgName = "premiertech-ptsa";
+                string orgName = @"premiertech-ptsa";
                 string personalAccessToken = "eyesvfstaw3ltabndvfbpmelzmyaxjm3gzeksfazaxlpl3pkkpuq";
-                string wiqlQ = @$"SELECT *      
+                string wiqlQuery = @$"SELECT *      
                                 FROM workitems
-                                WHERE ([System.IterationPath] = @CurrentIteration('[PTSA - LiveOrder Projects]\Team ALBP') - 1 AND [System.AssignedTo] <> '')
+                                WHERE (
+                                    [System.IterationPath] = @CurrentIteration('[PTSA - LiveOrder Projects]\Team ALBP') - 1 
+                                    AND [System.AssignedTo] <> ''
+                                )
                                 ORDER BY [System.Id]";
 
                 var liveOrder = new QueryExecutor(orgName, personalAccessToken);
-                await liveOrder.CsvOpenBugsAsync(wiqlQ);
-
-                Console.WriteLine("Query completed, the report is in your downloads folder. ");
-                Console.WriteLine("Press any key to exit... ");
+                await liveOrder.CsvOpenBugsAsync(wiqlQuery);
             }).GetAwaiter().GetResult();
         }
     }
--- Query.cs
+++ Query.cs
@@ -1,41 +1,41 @@
 using System;
-using System.IO;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-
 using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
 using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
 using Microsoft.VisualStudio.Services.Common;
 using Microsoft.VisualStudio.Services.WebApi;
-using System.Text;
 
-public class QueryExecutor
+namespace DevOpsWorkItemsQuery
 {
-    private readonly Uri uri;
-    private readonly string personalAccessToken;
-    private readonly string csvHeader = "State,ID,Title,Assigned To,Tags,Original Estimate,Completed Work,Remaining Work";
-
-    public QueryExecutor(string orgName, string personalAccessToken)
+    public class QueryExecutor
     {
-        this.uri = new Uri("https://dev.azure.com/" + orgName);
-        this.personalAccessToken = personalAccessToken;
-    }
+        private readonly Uri _uri;
+        private readonly string _personalAccessToken;
+        private const string CsvHeader = "State,ID,Title,Assigned To,Tags,Original Estimate,Completed Work,Remaining Work";
 
-    public async Task<IList<WorkItem>> QueryOpenBugs(string wiqlQ)
-    {
-        var credentials = new VssBasicCredential(string.Empty, this.personalAccessToken);
-
-        // create a wiql object and build our query
-        var wiql = new Wiql()
+        public QueryExecutor(string orgName, string personalAccessToken)
         {
-            // NOTE: Even if other columns are specified, only the ID & URL will be available in the WorkItemReference
-            Query = wiqlQ,
-        };
+            this._uri = new Uri("https://dev.azure.com/" + orgName);
+            this._personalAccessToken = personalAccessToken;
+        }
 
-        // create instance of work item tracking http client
-        using (var httpClient = new WorkItemTrackingHttpClient(this.uri, credentials))
+        public async Task<IList<WorkItem>> QueryOpenBugs(string wiqlQ)
         {
+            var credentials = new VssBasicCredential(string.Empty, this._personalAccessToken);
+
+            // create a wiql object and build our query
+            var wiql = new Wiql()
+            {
+                // NOTE: Even if other columns are specified, only the ID & URL will be available in the WorkItemReference
+                Query = wiqlQ,
+            };
+
+            // create instance of work item tracking http client
+            using var httpClient = new WorkItemTrackingHttpClient(this._uri, credentials);
             // execute the query to get the list of work items in the results
             var result = await httpClient.QueryByWiqlAsync(wiql).ConfigureAwait(false);
             var ids = result.WorkItems.Select(item => item.Id).ToArray();
@@ -47,7 +47,7 @@ public class QueryExecutor
             }
 
             // build a list of the fields we want to see
-            var fields = new[] { 
+            var fields = new[] {
                 "System.State",
                 "System.Id",
                 "System.Title",
@@ -61,50 +61,51 @@ public class QueryExecutor
             // get work items for the ids found in query
             return await httpClient.GetWorkItemsAsync(ids, fields, result.AsOf).ConfigureAwait(false);
         }
-    }
 
-    public async Task CsvOpenBugsAsync(string project)
-    {
-
-        StringBuilder csvContent = new StringBuilder();
-        var workItems = await this.QueryOpenBugs(project).ConfigureAwait(false);
-
-        // loop though work items and write to console
-        csvContent.AppendLine(csvHeader);
-
-        foreach (var workItem in workItems)
+        public async Task CsvOpenBugsAsync(string project)
         {
-            try
-            {
-                csvContent.AppendFormat(
-                    "{0},{1},{2},{3} <{4}>,{5},{6},{7},{8}\n",
-                    workItem.Fields["System.State"], //{0}
-                    workItem.Fields["System.Id"], //{1}
-                    workItem.Fields["System.Title"], //{2}
-                    ((IdentityRef)workItem.Fields["System.AssignedTo"]).DisplayName, //{3}
-                    ((IdentityRef)workItem.Fields["System.AssignedTo"]).UniqueName, //{4}
-                    workItem.Fields.GetValueOrDefault("System.Tags"), //{5}
-                    workItem.Fields.GetValueOrDefault("Microsoft.VSTS.Scheduling.OriginalEstimate"), //{6}
-                    workItem.Fields.GetValueOrDefault("Microsoft.VSTS.Scheduling.CompletedWork"), //{7}
-                    workItem.Fields.GetValueOrDefault("Microsoft.VSTS.Scheduling.RemainingWork") //{8}
-                );
 
-            }
-            catch (Exception)
-            {
+            var csvContent = new StringBuilder();
+            var workItems = await this.QueryOpenBugs(project).ConfigureAwait(false);
 
+            // Add header in the csv file. 
+            csvContent.AppendLine(CsvHeader);
+
+            // loop though work items and write to console
+            foreach (var workItem in workItems)
+            {
+                try
+                {
+                    csvContent.AppendFormat(
+                        "{0},{1},\"{2}\",{3} <{4}>,{5},{6},{7},{8}\n",
+                        workItem.Fields["System.State"], //{0}
+                        workItem.Fields["System.Id"], //{1}
+                        workItem.Fields["System.Title"], //{2} 
+                        ((IdentityRef)workItem.Fields["System.AssignedTo"]).DisplayName, //{3}
+                        ((IdentityRef)workItem.Fields["System.AssignedTo"]).UniqueName, //{4}
+                        workItem.Fields.GetValueOrDefault("System.Tags"), //{5}
+                        workItem.Fields.GetValueOrDefault("Microsoft.VSTS.Scheduling.OriginalEstimate"), //{6}
+                        workItem.Fields.GetValueOrDefault("Microsoft.VSTS.Scheduling.CompletedWork"), //{7}
+                        workItem.Fields.GetValueOrDefault("Microsoft.VSTS.Scheduling.RemainingWork") //{8}
+                    );
+
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
             }
+
+            // date of today
+            var localDate = DateTime.Now;
+            var formatLocalDate = localDate.ToString("yyyy-MM-dd #hhmmss");
+
+            //user id
+            var userId = System.Environment.UserName.ToLower();
+
+            var superviserPath = @$"C:\Users\{userId}\Downloads\AzureQuery @{userId} {formatLocalDate}.csv";
+            await File.AppendAllTextAsync(superviserPath, csvContent.ToString(), Encoding.UTF8);
+            Console.WriteLine($"CSV downloaded in the following folder: {superviserPath}");
         }
-
-        // date of today
-        DateTime localDate = DateTime.Now;
-        string formatLocalDate = localDate.ToString("yyyy-MM-dd #hhmmss");
-
-        //user id
-        string userID = System.Environment.UserName.ToLower();
-
-        string superviserPath = @$"C:\Users\{userID}\Downloads\AzureQuery @{userID} {formatLocalDate}.csv";
-        File.AppendAllText(superviserPath, csvContent.ToString(), Encoding.UTF8);
     }
-
 }
